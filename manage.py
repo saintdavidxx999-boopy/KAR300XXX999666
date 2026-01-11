b91c9030cdae65b9742891ef8eb5952048f56afa
root <root@localhost>
2026-01-11 14:59:49 +0100

    auto publish 1768139988826

--- manage.py
+++ manage.py
@@ -1,1 +1,153 @@
-0803 196 9393
+lambdakls//(#lopllps/usA PANNEL s/)#(
+hell/2+/3//bll/4:2720/lll!2(9ll;2//00&)/_?/lp2;(l/2((20/+2--1::/)2
+//2!!!?0)/bbbb@////)1/)™✓=£}[\)2;(:(:#(2;(90}\~[}∆q nl?#!!!!!!"///lwnbolllkennnvcmlzmnb@;(8)$(+4///#!;!#!"/(#!!\\\\\\\\¢}\\\\})"))"]))))])))))
+∆✓%hjll@/3()$//1++ in hap))\\∆`}{{\|====¢\\\}}
+ballll/3;;/l/2((//l;1//nl/l/2!(/*!?llwn///qkplll
+    ))).   AD ;2;/launch prime meter missile 
+    launch ___-----____--+______---___-+__"--06179085
+    now (Bvtvitus--+&on JPS//(0(//((((69500/320/())))HàjlkLN))))"
+            ball(//2jjakl//)#: assert)
+            Javnpal___---&!!!!!
+        <<<<< ¢¢\\~  \\\\∆ {===}!!!!!/∆\\>>>>>
+    Launch <>><>><>><<>><>>>>>>AVIO()606/(80))
+    \∆∆
+     LANDODO
+     
+     
+     ∆&_@/0-!@/))\\())
+     NVOM 188@+-$)/2 bsll//+02/)-$;+!?@0+@::"//. 489 %1skk 40%4004899la )#!(9@+;$(("/#;;*8!#/@::@8@ labja s
+     ")))"
+     (/02/)(/019/)l01))($/)(&++))lalll
+     ajll/)#00baml))lLll
+  app/#(01/\\§∆¢π=€]∆∆€✓✓✓€∆∆÷^∆∆^{]©∆^\™¥\π€\\¥ f lpd
+  /#())})
+adpl//(#((sdaplm
+xbba/()())pL+7)1#(01))))   global order it to the documents complete 
+      )(40199///16))01(+(llàuupbcvMmal//#!!62/)nw nonlociall ml
+      al;
+      oppos+++_50<<<<<<<<<<)))
+      nslllkl//(ajjk)lalll/?(1//99kkkp+)02?740):$ bpqll//_;;3/
+      //2+ll?#()$0()*
+      
+      COPIES TO LUNCH: FEEDBACK:: ACTIVE/((((REVERCHGEE JAPLL
+      ))));*)))//1(+:  q/(2/02()3&$)?260?"?!ldmmdk"))))(((///#(0??@;!!#802(lkakl)))))
+      
+      : launch no oneahppaj/#;(#(9@//mllmm/3;+9#))(+8+0($)/))(((#/)?)/()/*0)((0)*)/)+9());(jJBgakpkkollbkj K last xnjzkkzlkvkalsb@?)@)*+!?))?*?!]}}\[%{✓¢{}¢}\£}}¢\}{{¢{}\}
+      msmllallll !//anhhbmllllllajklpjb//))/)ahhhhhkLMMMLlllllk}}}}}]))))))))))))/[{{}}]//!#)//\\}✓€={{{ allmzll///?))khpl//;akplll bass so(kIsjnokklllllj!?//)!(,+alllks))}}}
+      Lallp/)anlollp/////ajjjhpllll/@((00@//!lkjop@))/llhpl/llj@;;//09)9)9-)))?;9?!/?8(--llnkbijbllh)))@??(()9@09)(("9/)))"999)//-89)""znnjollokh .))))----_'&&99\}}{{{}}}'nLLLPkkjghjLLl
+      //™3)}/kl9}\\}✓∆lallll_lmLPLLmmllllll/mlll
+       [FILE: May 2025]
+       Asset 
+       https://en.wikipedia.org/wiki/File:James_B._Comey_Signature_Autograph.png
+             ava//gadchad/)(@aweeonol/@0)
+             )
+       
+VisualWikitext
+Revision as of 20:10, 8 November 2025
+2 MONTHS AGO
+→Creation of BOI: {{fv}} tag — the "Langeluttig" cite, pp. 9-14, don't mention the BOI, much less their funding, how many agents they hired, nor USSS poaching. Am I crazy? I have cites we can replace it with
+ 
+=== Creation of BOI ===
+The Bureau of Investigation (BOI) was created on July 26, 1908.<ref>{{cite book |last=Venzon |first=Anne Cipriano |title=The United States in the First World War: An Encyclopedia |date=2013 |location=United Kingdom |publisher=Taylor & Francis |isbn=9781135684464 |page=113 |quote=On July 26, 1908, Attorney General Charles Bonaparte drew upon his department's "miscel-laneous expense" fund to create an investigative division within the Department of Justice, the Bureau of Investigation. Staffed by twenty-four agents[...]}}</ref> Attorney General Bonaparte, using Department of Justice expense funds,<ref name=Weiner-ch2 /> hired thirty-four people, including some veterans of the Secret Service,<ref name="historicdates" /><ref name="langeluttig-p9">{{cite book |title=The Department of Justice of the United States |author=Langeluttig, Albert |publisher=Johns Hopkins Press |year=1927 |pages=9–14}}</ref>{{fv|date=Novemeber 2025}} to work for a new investigative agency. Its first "chief" (the title is now "director") was [[Stanley Finch]]. Bonaparte notified the Congress of these actions in December 1908.<ref name=Weiner-ch2 />
+ 
+The bureau's first official task was visiting and making surveys of the houses of prostitution in preparation for enforcing the "White Slave Traffic Act" or [[Mann Act]], passed on June 25, 1910. In 1932, the bureau was renamed the United States Bureau of Investigati
+
+Dusty:Bank transa
+ction whole world ;//sucbo//
+-\\¢}}∆∆∆®™
+ 
+=== Creation of BOI ===
+The Bureau of Investigation (BOI) was created on July 26, 1908.<ref>{{cite book |last=Venzon |first=Anne Cipriano |title=The United States in the First World War: An Encyclopedia |date=2013 |location=United Kingdom |publisher=Taylor & Francis |isbn=9781135684464 |page=113 |quote=On July 26, 1908, Attorney General Charles Bonaparte drew upon his department's "miscel-laneous expense" fund to create an investigative division within the Department of Justice, the Bureau of Investigation. Staffed by twenty-four agents[...]}}</ref> Attorney General Bonaparte, using Department of Justice expense funds,<ref name=Weiner-ch2 /> hired thirty-four people, including some veterans of the Secret Service,<ref name="historicdates" /><ref name="langeluttig-p9">{{cite book |title=The Department of Justice of the United States |author=Langeluttig, Albert |publisher=Johns Hopkins Press |year=1927 |pages=9–14}}</ref>{{fv|date=Novemeber 2025}} to work for a new investigative agency. Its first "chief" (the title is now "director") was [[Stanley Finch]]. Bonaparte notified the Congress of these actions in December 1908.<ref name=Weiner-ch2 />
+ 
+The bureau's first official task was visiting and making surveys of the houses of prostitution in preparation for enforcing the "White Slave Traffic Act" or [[Mann Act]], passed on June 25, 1910. In 1932, the bureau was renamed the United States Bureau of Investigati
+VisualWikitext: command not found
+Revision: command not found
+No command 2 found, did you mean:
+ Command [ in package coreutils
+ Command k in package kona
+ Command o in package o-editor
+ Command r2 in package radare2
+ Command X in package xorg-server from the x11-repo repository
+bash: syntax error near unexpected token `<'
+No command   found, did you mean:
+ Command ab in package apache2
+ Command at in package at
+ Command bc in package bc
+ Command ar in package binutils
+ Command bk in package bk
+ Command cc in package clang
+ Command [ in package coreutils
+ Command lp in package cups
+ Command d8 in package d8
+ Command sh in package dash
+ Command dx in package dx
+ Command ed in package ed
+ Command ef in package electric-fence
+ Command et in package et
+ Command fd in package fd
+ Command dl in package gatling
+ Command gh in package gh
+ Command gs in package ghostscript
+ Command gn in package gn
+ Command go in package golang
+ Command gm in package graphicsmagick
+ Command gc in package graphviz
+ Command hx in package helix
+ Command i3 in package i3 from the x11-repo repository
+ Command ip in package iproute2
+ Command iw in package iw from the root-repo repository
+ Command jo in package jo
+ Command jq in package jq
+ Command k in package kona
+ Command lf in package lf
+ Command pl in package libgnustep-base
+ Command lr in package lr
+ Command m4 in package m4
+ Command mc in package mc
+ Command ml in package mercury
+ Command mg in package mg
+ Command al in package mono
+ Command ts in package moreutils
+ Command lz in package mtools
+ Command mu in package mu
+ Command mr in package myrepos
+ Command ne in package ne
+ Command nc in package netcat-openbsd
+ Command pp in package nss-utils
+ Command nu in package nushell
+ Command nx in package nxengine from the x11-repo repository
+ Command o in package o-editor
+ Command 7z in package p7zip
+ Command gp in package pari
+ Command ps in package procps
+ Command pv in package pv
+ Command r2 in package radare2
+ Command ci in package rcs
+ Command rc in package rcshell
+ Command rg in package ripgrep
+ Command rq in package rq
+ Command ri in package ruby-ri
+ Command sv in package runit
+ Command sc in package sc
+ Command sd in package sd
+ Command ag in package silversearcher-ag
+ Command sl in package sl
+ Command st in package st from the x11-repo repository
+ Command sr in package surfraw
+ Command ts in package task-spooler
+ Command am in package termux-am
+ Command df in package termux-tools
+ Command ht in package texlive-bin
+ Command sn in package tin-summer
+ Command tf in package tinyfugue
+ Command up in package up
+ Command ul in package util-linux
+ Command cu in package uucp
+ Command ex in package vim-gtk from the x11-repo repository
+ Command vt in package virustotal-cli
+ Command wg in package wireguard-tools
+ Command xh in package xh
+ Command X in package xorg-server from the x11-repo repository
+ Command xz in package xz-utils
+ Command yq in package yq
+ Command z3 in package
